031509477e059a732ce3ce26ea1dec0eba0e4dc0
Anderson Yukihiro Yamakawa <63267003+Krosytl@users.noreply.github.com>
2022-03-24 22:48:58 -0300

    Initial commit

---
+++ src/components/Taskitem/Taskitem.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./task-item.css";
+
+export default function Taskitem({
+  id,
+  title,
+  taskState,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editableTitle, setEditableTitle] = useState(title);
+
+  const ontitleChange = (event) => {
+    const newTitle = event.target.value;
+    setEditableTitle(newTitle);
+    onTaskUpdate(id, newTitle, taskState);
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setIsEditing(false);
+      if (editableTitle.length === 0) {
+        onDeleteTask(id);
+      }
+    }
+  };
+
+  const ontaskStateChange = (event) => {
+    onTaskUpdate(id, title, event.target.value);
+  };
+
+  if (isEditing) {
+    return (
+      <div className="task-item">
+        <input
+          type="text"
+          value={editableTitle}
+          onChange={ontitleChange}
+          onKeyPress={onKeyPress}
+        />
+      </div>
+    );
+  } else
+    return (
+      <div className="task-item">
+        <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>
+        <select onChange={ontaskStateChange} value={taskState}>
+          <option value="Pendente">Pedente</option>
+          <option value="Fazendo">Fazendo</option>
+          <option value="Completa">Completa</option>
+        </select>
+      </div>
+    );
+}
+
+Taskitem.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  taskState: PropTypes.string.isRequired
+};
---
+++ src/components/Tasklist/Tasklist.js
@@ -0,0 +1,49 @@
+import "./tasklist.css";
+import propTypes from "prop-types";
+import plusIcon from "../../img/plus-icon.svg";
+
+import Taskitem from "../Taskitem/Taskitem";
+
+export default function Tasklist({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const addTask = () => {
+    onAddTask("Nova Tarefa", taskState);
+  };
+
+  return (
+    <div className="tasklist">
+      <div className="title">{title}</div>
+      <div className="content">
+        {tasks.map((task) => {
+          return (
+            <Taskitem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              taskState={task.state}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
+            />
+          );
+        })}
+        {tasks.length === 0 && <div className="empty-list">Lista Fazia</div>}
+        <button onClick={addTask} className="btn">
+          <img src={plusIcon} alt="plus" />
+          Adicionar Tarefa
+        </button>
+      </div>
+    </div>
+  );
+}
+
+Tasklist.propTypes = {
+  title: propTypes.string.isRequired,
+  onAddTask: propTypes.func.isRequired,
+  tasks: propTypes.array.isRequired
+};
